472dc8820083a63ffaaba5fdea7a9ab9c7a5bdd5
amelchenko_n <amelchenko_n@stilsoft.ru>
2023-05-26 10:12:20 +0300

    init

--- src/App.vue
+++ src/App.vue
@@ -5,5 +5,5 @@ console.log('[App.vue]', `Hello world from Electron ${process.versions.electron}
 </script>
 
 <template>
-	<h1>App running Electron + Vite + Vue 3</h1>
+	<h1 class="text-4xl">App running Electron + Vite + Vue 3</h1>
 </template>
