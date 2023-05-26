a5fe7891253bd6ba64e0f52eb44dee6e0b81ae69
amelchenko_n <amelchenko_n@stilsoft.ru>
2023-05-26 09:41:40 +0300

    init

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import { onMounted, ref } from 'vue';
+
+console.log('[App.vue]', `Hello world from Electron ${process.versions.electron}!`);
+</script>
+
+<template>
+	<h1>App running Electron + Vite + Vue 3</h1>
+</template>
